ad2866d65e788b6a520ec6b8969523b000ad0882
程金盛 <jelly@chengjinshengdeMacBook-Pro.local>
2016-07-27 15:23:14 +0800

    zfb login

--- src/Zfb/Config/alipay.config.php
+++ src/Zfb/Config/alipay.config.php
@@ -16,8 +16,8 @@
 	'input_charset'=> strtolower('utf-8'),
 
 	//ca证书路径地址，用于curl中ssl校验
-	//请保证cacert.pem文件在当前文件夹目录中
-	'cacert'    => getcwd().'\\cacert.pem',
+	//请保证cacert.pem文件在当前文件夹目录中  getcwd().'\\cacert.pem'
+	'cacert'    => __DIR__.'/cacert.pem',
 
 	//访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
 	'transport'    => 'http',
