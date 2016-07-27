3e71db4dad35e9ded5c19a889d4738c0b31eab9c
程金盛 <jelly@chengjinshengdeMacBook-Pro.local>
2016-07-27 12:20:59 +0800

    zfb login

--- demo/zfb_login.config.php
+++ demo/zfb_login.config.php
@@ -2,7 +2,7 @@
 
 return [
     'partner'=>'2088121461512731',
-    'key'=>'xccvtqfcbcoq4hd2b5sfbdtlxjlhvdcf',
+    'key'=>'xccvtqfcbcoq4hd2b5sfbdtlxabchvdcf',
     'seller_id'=>'2088121461512731',
 	'target_service' => "user.auth.quick.login", //固定写法
     // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
